767e82249b312055fee6473b8ab679605e342a19
Giovahnni Leon <130669783+P3RULUCID@users.noreply.github.com>
2024-10-06 14:59:52 -0400

    Update ShoppingCart.py
    modify_item function improved to ensure modifications are only done if item exists or is not equal to default values

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -18,18 +18,20 @@ class ShoppingCart:
         print("Item not found in cart. Nothing removed.")
 
     def modify_item(self, modified_item: ItemToPurchase):
+        item_found = False
         for item in self.cart_items:
             if item.item_name == modified_item.item_name:
+                item_found = True              
                 if modified_item.item_price != 0:
                     item.item_price = modified_item.item_price
                 if modified_item.item_quantity != 0:
                     item.item_quantity = modified_item.item_quantity
                 if modified_item.item_description != "none":
-                    item.item_description = modified_item.item_description
-                    print(f"{modified_item.item_name} has been updated.")
-            else:
-                print("Item not found in cart. Nothing modified.")
-                return
+                    item.item_description = modified_item.item_description          
+                print(f"{modified_item.item_name} has been updated.")
+                break           
+        if not item_found:
+            print("Item not found in cart. Nothing modified.")
 
     def get_num_items_in_cart(self):
         return sum(item.item_quantity for item in self.cart_items)
